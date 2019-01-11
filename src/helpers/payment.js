b2e045f006ad89595833888fe9f840260d9de2aa
nurul irfan <irfanlongstride@gmail.com>
2019-01-11 08:54:09 +0700

    deploy 1st

--- src/helpers/payment.js
+++
@@ -1,9 +0,0 @@
-export default {
-  moviePrice (rating) {
-    rating = parseFloat(rating)
-    if (rating >= 8) return 21250
-    else if (rating >= 6) return 16350
-    else if (rating >= 3) return 8250
-    else return 3500
-  }
-}
